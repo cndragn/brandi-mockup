16bbdcf7cc2d80a2f34b49f9595e85678677d0a6
Candice Davidson <cndragn@gmail.com>
2018-05-29 14:00:27 -0400

    style jumbotron social

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Background from '../images/tealbg.jpg';
 
+var FontAwesome = require('react-fontawesome');
+
 var sectionStyle = {
     backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${Background})`
   };
@@ -9,16 +11,19 @@ var sectionStyle = {
 export class Jumbotron extends React.Component {
     render() {
         return (
-            <div style={ sectionStyle } class="jumbotron">
+            <div style={ sectionStyle } className="jumbotron">
                 <div class="container text">
                     <div>
                     <h1>Meet <span>Brandi</span>!</h1> 
                     <h2><span>/creative</span> one page template</h2> 
-                    <h3 class="lined"><span>We are a team of professionals</span></h3>
+                    <h3 className="lined"><span>We are a team of professionals</span></h3>
                     </div>
                 </div>
-                <div class="container social">
-                    Twitter | Facebook | Google+ | Dribbl
+                <div className="container social">
+                <a href="#"><FontAwesome name='twitter' /></a>
+                <a href="#"><FontAwesome name='facebook-f' /></a>
+                <a href="#"><FontAwesome name='google-plus' /></a>
+                <a href="#"><FontAwesome name='dribbble' /></a>
                 </div>
             </div>
         );
