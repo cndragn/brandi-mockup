56fe4936ebb83ff13f8d7141f60d18b2faf7bb30
Candice Davidson <cndragn@gmail.com>
2018-06-03 23:28:06 -0400

    style Features section icons

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import {Divider} from './Divider';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faFeather from '@fortawesome/fontawesome-free-solid/faFeather'
+import faPencil from '@fortawesome/fontawesome-free-solid/faPencilAlt'
+import faBullhorn from '@fortawesome/fontawesome-free-solid/faBullhorn'
 
 export class Features extends React.Component {
     render () {
@@ -9,17 +13,34 @@ export class Features extends React.Component {
                     <h2>Features</h2>
                     <Divider />
                     <div class="container-fluid row">
-                        <div class="col">
-                        <h3>Branding</h3>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
+                        <div class="col-md-4">
+                            <div>
+                                <FontAwesomeIcon icon={faFeather} />
+                            </div>
+                            <div>
+                                <h3>Branding</h3>                                
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
+                            </div>
                         </div>
-                        <div class="col">
-                        <h3>Development</h3>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
+                        
+                        <div class="col-md-4">
+                            <div>
+                                <FontAwesomeIcon icon={faPencil} />
+                            </div>
+                            <div>
+                                <h3>Development</h3>                                
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
+                            </div>
                         </div>
-                        <div class="col">
-                        <h3>Consulting</h3>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
+                        
+                        <div class="col-md-4">
+                            <div>
+                                <FontAwesomeIcon icon={faBullhorn} />
+                            </div>
+                            <div>
+                                <h3>Consulting</h3>                                
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
+                            </div>
                         </div>
                     </div>
                 </section>
