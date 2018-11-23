013488d195545f535ee3b062e7110d2b55dc15af
Candice Davidson <cndragn@gmail.com>
2018-11-23 09:41:42 -0800

    setup travis ci/cd

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -4,29 +4,41 @@ import Background from '../images/tealbg.jpg';
 var FontAwesome = require('react-fontawesome');
 
 var sectionStyle = {
-    backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${Background})`
-  };
-
+	backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${Background})`
+};
 
 export class Jumbotron extends React.Component {
-    render() {
-        return (
-            <div style={ sectionStyle } className="jumbotron">
-                <div className="container text">
-                    <div>
-                    <h1>Meet <span>Brandi</span>!</h1> 
-                    <h2><span>/creative</span> one page template</h2> 
-                    <h3 className="lined"><em>We are a team of professionals</em></h3>
-                    </div>
-                </div>
-                <div className="container social">
-                <a href="http://www.twitter.com"><FontAwesome name='twitter' /></a>
-                <a href="http://www.facebook.com"><FontAwesome name='facebook-f' /></a>
-                <a href="http://www.google.com"><FontAwesome name='google-plus' /></a>
-                <a href="http://www.google.com"><FontAwesome name='dribbble' /></a>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div style={sectionStyle} className="jumbotron">
+				<div className="container text">
+					<div>
+						<h1>
+							Meet <span>Brandi</span>!
+						</h1>
+						<h2>
+							<span>/creative</span> one page template
+						</h2>
+						<h3 className="lined">
+							<em>We are a team of professionals!</em>
+						</h3>
+					</div>
+				</div>
+				<div className="container social">
+					<a href="http://www.twitter.com">
+						<FontAwesome name="twitter" />
+					</a>
+					<a href="http://www.facebook.com">
+						<FontAwesome name="facebook-f" />
+					</a>
+					<a href="http://www.google.com">
+						<FontAwesome name="google-plus" />
+					</a>
+					<a href="http://www.google.com">
+						<FontAwesome name="dribbble" />
+					</a>
+				</div>
+			</div>
+		);
+	}
 }
-
