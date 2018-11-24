c3c5e1f562500d84a6a3be7d47b94a0b5a8a08fb
Candice Davidson <cndragn@gmail.com>
2018-11-23 19:25:24 -0800

    style team bio

--- src/components/Team.js
+++ src/components/Team.js
@@ -5,6 +5,8 @@ import Man01 from '../images/male-model-2911329.jpg';
 import Man02 from '../images/laughing-man-1690965.jpg';
 import Man03 from '../images/smiling-man-888532.jpg';
 
+var FontAwesome = require('react-fontawesome');
+
 class Team extends React.Component {
 	render() {
 		return (
@@ -20,28 +22,68 @@ class Team extends React.Component {
 					<div className="container-fluid row text-center">
 						<div className="col-md-3">
 							<div className="info">
-								<img src={Man01} alt="" />
+								<div className="bio">
+									<img src={Man01} alt="" />
+									<div className="bio-text">
+										Loren gypsum dolor sit mate, time am tied. Ea alien um accustoms quo.
+									</div>
+									<div className="bio-fa">
+										<FontAwesome name="twitter" />
+										<FontAwesome name="facebook" />
+										<FontAwesome name="google-plus" />
+									</div>
+								</div>
 								<p>John Filmr Doe</p>
 							</div>
 							<p>Managing Director</p>
 						</div>
 						<div className="col-md-3">
 							<div className="info">
-								<img src={Woman} alt="" />
+								<div className="bio">
+									<img src={Woman} alt="" />
+									<div className="bio-text">
+										Loren gypsum dolor sit mate, time am tied. Ea alien um accustoms quo.
+									</div>
+									<div className="bio-fa">
+										<FontAwesome name="twitter" />
+										<FontAwesome name="facebook" />
+										<FontAwesome name="google-plus" />
+									</div>
+								</div>
 								<p>Chrystine Hineu</p>
 							</div>
 							<p>Lead Designer</p>
 						</div>
 						<div className="col-md-3">
 							<div className="info">
-								<img src={Man02} alt="" />
+								<div className="bio">
+									<img src={Man02} alt="" />
+									<div className="bio-text">
+										Loren gypsum dolor sit mate, time am tied. Ea alien um accustoms quo.
+									</div>
+									<div className="bio-fa">
+										<FontAwesome name="twitter" />
+										<FontAwesome name="facebook" />
+										<FontAwesome name="google-plus" />
+									</div>
+								</div>
 								<p>Martin Matrone</p>
 							</div>
 							<p>Lead Developer</p>
 						</div>
 						<div className="col-md-3">
 							<div className="info">
-								<img src={Man03} alt="" />
+								<div className="bio">
+									<img src={Man03} alt="" />
+									<div className="bio-text">
+										Loren gypsum dolor sit mate, time am tied. Ea alien um accustoms quo.
+									</div>
+									<div className="bio-fa">
+										<FontAwesome name="twitter" />
+										<FontAwesome name="facebook" />
+										<FontAwesome name="google-plus" />
+									</div>
+								</div>
 								<p>Steve Flaulkin</p>
 							</div>
 							<p>Sr. UI Designer</p>
